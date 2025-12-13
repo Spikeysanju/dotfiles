d247485764745c964545650d0f7ea90b286daa5a
Sanju Sivalingam <spikeysanju98@gmail.com>
2025-12-13 07:29:28 +0530

    add secrets

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,42 @@
 # --- ENVIRONMENT VARIABLES
+
+# User info
 export DEFAULT_USER="sanju"
+
+# Editor preferences
+export EDITOR="hx"  # or "nvim", "vim", "code", etc.
+export VISUAL="$EDITOR"
+
+# Language/Development
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"    # Helix installer
+
+# Node.js (if using nvm, this is usually handled by nvm)
+# export NODE_VERSION="20"
+
+# Python (if using pyenv, this is usually handled by pyenv)
+# export PYTHON_VERSION="3.11"
+
+# Go (if installed)
+# export GOPATH="$HOME/go"
+# export PATH="$GOPATH/bin:$PATH"
+
+# Rust (if installed)
+# export PATH="$HOME/.cargo/bin:$PATH"
+
+# Docker
+# export DOCKER_BUILDKIT=1
+# export COMPOSE_DOCKER_CLI_BUILD=1
+
+# Git
+# export GIT_EDITOR="$EDITOR"
+
+# History
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILE="$HOME/.zsh_history"
+
+# Add more environment variables below as needed
+# export MY_CUSTOM_VAR="value"
 
